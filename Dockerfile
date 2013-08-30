963aec9e8ba00dbce685e3dcbdc11477c0c05ca7
Noah Isaacson <noah@nisaacson.com>
2013-08-30 16:39:00 -0600

    Add Vagrantfile

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,28 @@ FROM ubuntu:12.10
 MAINTAINER Terin Stock <terinjokes@gmail.com>
 
 # Setup image
+RUN sudo apt-get update
+RUN sudo apt-get install -y software-properties-common build-essential
+RUN sudo add-apt-repository ppa:chris-lea/node.js
+RUN sudo apt-get update
+RUN sudo apt-get install -y nodejs
+
+
+
+# Install couchdb
+RUN sudo apt-add-repository ppa:nilya/couchdb-1.3
+RUN sudo apt-get update
+RUN sudo apt-get install -y couchdb
+RUN sudo apt-get install -y curl wget git
+RUN npm install -g npm
+RUN echo "install couchapp"
+RUN npm install -g couchapp
+
 ADD scripts/setup.sh /root/setup-npm.sh
 RUN /root/setup-npm.sh
 
 # Runtime configuration
+
 ADD scripts/startup.sh /root/startup-npm.sh
 CMD /root/startup-npm.sh
 
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Setting up Node.js apt repository
-echo "deb http://ppa.launchpad.net/chris-lea/node.js/ubuntu precise main" >> /etc/apt/sources.list
-gpg --ignore-time-conflict --no-options --no-default-keyring --secret-keyring /tmp/tmp.U6rnskPajB --trustdb-name /etc/apt/trustdb.gpg --keyring /etc/apt/trusted.gpg --primary-keyring /etc/apt/trusted.gpg --keyserver hkp://keyserver.ubuntu.com:80/ --recv 136221EE520DDFAF0A905689B9316A7BC7917B12
+# # Setting up Node.js apt repository
+# echo "deb http://ppa.launchpad.net/chris-lea/node.js/ubuntu precise main" >> /etc/apt/sources.list
+# gpg --ignore-time-conflict --no-options --no-default-keyring --secret-keyring /tmp/tmp.U6rnskPajB --trustdb-name /etc/apt/trustdb.gpg --keyring /etc/apt/trusted.gpg --primary-keyring /etc/apt/trusted.gpg --keyserver hkp://keyserver.ubuntu.com:80/ --recv 136221EE520DDFAF0A905689B9316A7BC7917B12
 
-# Apt Installation
-#
-# Installing git, nodejs, curl and couchdb
-apt-get update -y -q
-apt-get install -y -q nodejs git curl couchdb
+# # Apt Installation
+# #
+# # Installing git, nodejs, curl and couchdb
+# apt-get update -y -q
+# apt-get install -y -q nodejs git curl couchdb
 
 # Couchdb Configuration
 #
@@ -21,15 +21,28 @@ echo -e "[httpd]\nsecure_rewrites = false" >> /etc/couchdb/local.ini
 # Install npmjs.org project
 git clone https://github.com/isaacs/npmjs.org.git /var/npmjs.org
 cd /var/npmjs.org
-npm install couchapp -g
-npm install couchapp semver
+npm install semver
+# npm install couchapp semver
 
 # Configure npmjs.org
-couchdb &
+sudo service couchdb start
+echo "sleep until couchdb is online"
+sleep '5s'
+URL=localhost:5948
+while true
+do
+  echo "try to connect to couchdb at url $URL"
+  curl $URL
+  if [ $? -eq 0 ]; then
+    break
+  fi
+  sleep "1s"
+done
+
 echo "> Creating registry DB" >&1
 curl -X PUT http://localhost:5984/registry
 echo "> Pushing Apps" >&1
-couchapp push registry/shadow.js http://localhost:5984/registry
+# couchapp push registry/shadow.js http://localhost:5984/registry
 couchapp push registry/app.js http://localhost:5984/registry
 npm set _npmjs.org:couch=http://localhost:5984/registry
 echo "> Loading views" >&1
